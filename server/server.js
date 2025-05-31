a2b6ef4d69b9e82e9d79c04f49173fc120095422
shayoniA <sayania662@gmail.com>
2025-05-31 23:27:02 +0530

    ServerJS modified

--- server/server.js
+++ server/server.js
@@ -17,12 +17,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from React
-app.use(express.static(path.join(__dirname, '../bookbuzz/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../bookbuzz/build', 'index.html'));
-});
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
@@ -56,6 +50,12 @@ app.get('/api/users/:userId/favorites', async (req, res) => {
     console.error('Error fetching favorites:', error);
     res.status(500).json({ message: 'Server error' });
   }
+});
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, '../bookbuzz/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../bookbuzz/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
