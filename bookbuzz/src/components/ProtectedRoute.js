0918877503d8a9c9469e6d0a9512c087228b9931
shayoniA <sayania662@gmail.com>
2025-06-01 13:18:01 +0530

    UpdatedProtectedRoute

--- bookbuzz/src/components/ProtectedRoute.js
+++ bookbuzz/src/components/ProtectedRoute.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { auth } from '../firebase';
 
+// const ProtectedRoute = ({ children }) => {
+//   const user = auth.currentUser;
+//   return user ? children : <Navigate to="/login" />;
+// };
+
 const ProtectedRoute = ({ children }) => {
-  const user = auth.currentUser;
-  return user ? children : <Navigate to="/login" />;
+  const isLoggedIn = localStorage.getItem('userId');  // Check localStorage
+  return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
